91686146233c4707888159728dae3698580dea3a
FirstWisdom <jason.zelaya21@yahoo.com>
2019-08-30 21:58:59 -0700

    add css and comments to html

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+// Game Plan
+// 
+// add any dice image as the src for both <img> in index.html
+// get .dice1 and .dice2 to randomly change images every time the page is refreshed
+// generate random "rolls" for image swaps
+// change <h1> to display the winner or a draw
