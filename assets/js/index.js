dfe2906bb57815b7f986e607f01ba417a8b1d7d9
FirstWisdom <jason.zelaya21@yahoo.com>
2019-09-08 07:51:32 -0700

    dicee complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,32 @@
-// Game Plan
-//
-// get .dice1 and .dice2 to randomly change images every time the page is refreshed
-// change <h1> to display the winner or a draw
-
-
-
-
-// Create a random number from 1-6
+// PLAYER 1
+// Create a random number from 1-6 for Player 1 Dice
 var randomNum1 = Math.floor(Math.random() * 6) + 1;
-// Select an image based on the value of randomNum1
+// Select an image based on the value of randomNum1 for Player 1 Dice
 var randomDiceImg1 = 'assets/images/dice' + randomNum1 + '.png';
-
-// Create a random number from 1-6
-var randomNum2 = Math.floor(Math.random() * 6) + 1;
-// Select an image based on the value of randomNum1
-var randomDiceImg2 = 'assets/images/dice' + randomNum2 + '.png';
-
-// Set .img1's src value to the path value of randomDiceImg1
+// Random dice image for Player 1
 $('.img1').attr('src', randomDiceImg1);
-// Set .img2's src value to the path value of randomDiceImg2
+
+// PLAYER 2
+// Create a random number from 1-6 for Player 2 Dice
+var randomNum2 = Math.floor(Math.random() * 6) + 1;
+// Select an image based on the value of randomNum1 for Player 2 Dice
+var randomDiceImg2 = 'assets/images/dice' + randomNum2 + '.png';
+// Random dice image for Player 2
 $('.img2').attr('src', randomDiceImg2);
 
 
 
 
-//
+// If Player 1 wins
+if (randomNum1 > randomNum2) {
+  // Change h1 to Player 1 Wins!
+  $('h1').html('🚩 Player 1 Wins!');
+  // If Player 2 wins
+} else if (randomNum2 > randomNum1) {
+  // Change h1 to Player 2 Wins!
+  $('h1').html('Player 2 Wins! 🚩');
+  // If both dice have equal values
+} else {
+  // Change h1 to Draw!
+  $('h1').html('Draw!');
+}
